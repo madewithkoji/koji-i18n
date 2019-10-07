b2732df60767ad47ab3da64ce0f365712228ce65
sean <admin@koji-projects.com>
2019-10-06 19:16:25 -0700

    Update buildOrder.js

--- scripts/buildOrder.js
+++ scripts/buildOrder.js
@@ -3,7 +3,10 @@ const baseObject = require('../i18n/en/strings.json');
 
 const newKeys = Object.keys(baseObject).reduce((acc, cur) => {
   if (!object[cur]) {
-    const baseString = baseObject[cur].replace(/Koji/g, '[[[Koji]]]').replace(/Discord/g, '[[[Discord]]]');
+    const baseString = baseObject[cur]
+      .replace(/Koji/g, '[[[Koji]]]')
+      .replace(/Discord/g, '[[[Discord]]]')
+      .replace(/Google Poly/g, '[[[Google Poly]]]');
     acc.push(`[[[${cur}]]] ${baseString}`);
   }
   return acc;
